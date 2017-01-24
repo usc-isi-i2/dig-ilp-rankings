e66396bfbfeef5c8e029c7fd87b43df916433ede
Rahul Kapoor <rahulkapoor.bits@gmail.com>
2017-01-24 13:26:45 -0800

    Adding files for pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'digILPRankings',
+    'description': 'digILPRankings',
+    'author': 'Rahul Kapoor',
+    'url': 'https://github.com/usc-isi-i2/dig-ilp-rankings',
+    'download_url': 'https://github.com/usc-isi-i2/dig-ilp-rankings',
+    'author_email': 'rahulkap@isi.edu',
+    'version': '0.3.0',
+    'install_requires': ['digExtractor>=0.3.0'],
+    # these are the subdirs of the current directory that we care about
+    'packages': ['digIlpRankings'],
+    'scripts': [],
+}
+
+setup(**config)
